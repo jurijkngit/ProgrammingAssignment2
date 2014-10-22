021e7975d1a8a32e20bf43e1d9977ff237128532
Jurij Knuples <jurijkngit@gmail.com>
2014-10-22 17:55:26 +0200

    initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cached inversed matrix. 
 
-## Write a short comment describing this function
-
+## create CacheMatrix object r
 makeCacheMatrix <- function(x = matrix()) {
+  ## initialize inverse matrix,x is already set trough parameter transfer
+  m <- NULL
+  ## set new matrix and clear inversed matrix
+  set <- function(y) {
+    ## set x and m in outer frame 
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  ## set inverse matrix
+  setinverse <- function(x,...) m <<- solve(x,...)
+  getinverse <- function() m
+  ## return accessible elements
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse  )
+}   
 
-}
-
-
-## Write a short comment describing this function
-
+## get inverse matrix. If needed store it in object 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    ## already have inversed matrix
+    message("getting cached data")
+    return(m)
+  }
+  ## get matrix data
+  ## transfer remaining parameters to solve function inside object
+  ## set inverse matrix in object
+  x$setinverse(x$get(),...) 
+  ## return inverse matrix
+  x$getinverse()
 }
+
+## Test sample:
+## source("cachematrix.R")
+## m <- matrix(sample(-1000:1000,100),10,10)  
+## mc <- makeCacheMatrix(m)  
+## this should produce identity matrix (evaluate twice !)
+## mc$get() %*% cacheSolve(mc)
